2951c5c2684f0060dbbafc78fd39e420f3052fbf
Sam Wolfson <wolfson@cs.uw.edu>
2016-05-03 13:42:54 -0700

    Improved loading and debugging.

--- tophack.js
+++ tophack.js
@@ -4,34 +4,43 @@ var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 console.log("### TopHack v0.1 ###");
 
-setTimeout(function() {
+// define a new observer
+var obs = new MutationObserver(function(mutations, observer) {
+  // look through all mutations that just occured
+  for(var i = 0; i < mutations.length; i++) {
+    // look through all added nodes of this mutation
+    for(var j = 0; j < mutations[i].addedNodes.length; j++) {
+      var change = mutations[i].addedNodes[j];
+      console.log(change);
 
-  console.log("### TopHack loading complete. ###");
-
-  // define a new observer
-  var obs = new MutationObserver(function(mutations, observer) {
-    // look through all mutations that just occured
-    for(var i = 0; i < mutations.length; i++) {
-      // look through all added nodes of this mutation
-      for(var j = 0; j < mutations[i].addedNodes.length; j++) {
-        var change = mutations[i].addedNodes[j];
-        console.log(change);
-        // for some reason we have to wait a bit for questions to load
-        setTimeout(function() {
-          try {
-            var n = new Notification('New Top Hat Question', {
-              'body': change.querySelector('.thm_panel_body > .question_details_tab > #question_body > div > div > .question_container > table > tbody > tr > .question_description_content > div > p').textContent
-            });
-          } catch(e) {
-            console.log(e);
-          }
-        }, 500);
-      }
+      // for some reason we have to wait a bit for questions to load
+      setTimeout(function() {
+        try {
+          var n = new Notification('New Top Hat Question', {
+            'body': change.querySelector('.thm_panel_body > .question_details_tab > #question_body > div > div > .question_container > table > tbody > tr > .question_description_content > div > p').textContent
+          });
+        } catch(e) {
+          console.log(e);
+        }
+      }, 500);
     }
-  });
+  }
+});
 
-  // have the observer observe foo for changes in children
-  obs.observe($(".panels").get(2), {
-    childList: true
-  });
-}, 10000);
+
+// this doesn't immediately work, so persist until it does
+function startObserve() {
+  try {
+    // have the observer observe foo for changes in children
+    obs.observe($(".panels").get(2), {
+      childList: true
+    });
+    console.log("#### TopHack loading complete. ####")
+  } catch(e) {
+    console.log("#### TopHack observer hook failed, trying again in 2 seconds. ####");
+    console.log(e);
+    setTimeout(startObserve, 2000);
+  }
+}
+
+startObserve();
